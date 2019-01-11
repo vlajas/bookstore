232706ce5f1eb807f420e6cd01c06432a457049e
Milan Jovanović <milannis.995@gmail.com>
2019-01-11 19:52:10 +0100

    - Implemented UserController with CRUD operations

---
+++ bookstore.Server/Controllers/UserController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using bookstore.Shared.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bookstore.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private readonly BookstoreDbContext _context;
+
+        public UserController(BookstoreDbContext context)
+        {
+            _context = context;
+        }
+
+        [Route("[action]")]
+        public List<User> Get()
+        {
+            List<User> users = _context.User.ToList();
+
+            return users;
+        }
+
+        [Route("[action]/{id?}")]
+        public User Get(int id)
+        {
+            User user = _context.User.Find(id);
+
+            return user;
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public bool Create([FromBody]User user)
+        {
+            // TODO: Add validation logic
+
+            try
+            {
+                _context.User.Add(user);
+
+                _context.SaveChanges();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                // TODO: Add exception logging?
+                
+                return false;
+            }
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public bool Update([FromBody]User user)
+        {
+            try
+            {
+                _context.User.Update(user);
+
+                _context.SaveChanges();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                // TODO: Add exception logging?
+
+                return false;
+            }
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public bool Delete(int id)
+        {
+            User user = _context.User.Find(id);
+
+            if (user == null)
+            {
+                return false;
+            }
+
+            try
+            {
+                _context.User.Remove(user);
+
+                _context.SaveChanges();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                // TODO: Add exception logging?
+
+                return false;
+            }
+        }
+    }
+}
